ca63af6c81552b03e62e89de4a1c948358ce14df
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-19 16:49:23 -0700

    add home page slogan

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="landing-page-container">test</div>;
+  return (
+    <div className="landing-page-container">
+      <div className="homepage-slogan">
+        <div className="slogan-line">Go fast,</div>
+        <div className="slogan-line">break things,</div>
+        <div className="slogan-line">raise money</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
