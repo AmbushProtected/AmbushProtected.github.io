32ed7968337c5da9a79f87c7030a6e475a0b5c76
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-25 17:22:05 -0700

    input initial spacing of navbar links

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,8 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar-container">
-        <div className="navbar-name">Ambush Protected</div>
-        <div className="navbar-contact">Contact Us</div>
+        <div className="navbar-content">Ambush Protected</div>
+        <div className="navbar-content">Contact Us</div>
       </div>
     );
   }
