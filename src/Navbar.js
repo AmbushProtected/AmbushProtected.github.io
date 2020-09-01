3c85955d8b97d6c848a94dbbbfc8a2936d5ebae2
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-31 17:23:55 -0700

    remove unused class

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,9 +8,11 @@ class Navbar extends Component {
       <div className="navbar-container">
         <ScrollLink className="navbar-logo-container" to="main">
           <img src={logo} alt="" className="logo" />
-          <div className="navbar-content">Ambush Protected</div>
+          <div className="navbar-content navbar-logo-name">
+            Ambush Protected
+          </div>
         </ScrollLink>
-        <ScrollLink className="navbar-content navbar-logo-name" to="contact">
+        <ScrollLink className="navbar-content" to="contact">
           Contact Us
         </ScrollLink>
       </div>
